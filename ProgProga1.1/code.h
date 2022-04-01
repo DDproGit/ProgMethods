90843dea08cdc65718e5648ae340a14e0bfe522b
NSigarev <nikita.sigarev@mail.ru>
2022-04-02 00:10:02 +0700

    Added a project

---
+++ ProgProga1.1/code.h
@@ -0,0 +1,63 @@
+#pragma once
+#include <vector>
+#include <iostream>
+
+struct Shape1
+{
+	struct sphere
+	{
+		int radius;
+	};
+	struct parallelepiped
+	{
+		int heigth;
+		int width;
+		int depth;
+	};
+	union
+	{
+		sphere mySphere;
+		parallelepiped myParallelepiped;
+	};
+};
+
+class Shape
+{
+public:
+	Shape();
+	Shape(int radius);
+	Shape(int heigth, int width, int depth);
+	~Shape();
+	void setValues(int radius);
+	void setValues(int heigth, int width, int depth);
+	int getRadius();
+	int getHeigth();
+	int getWidth();
+	int getDepth();
+protected:
+	int radius;
+	int heigth;
+	int width;
+	int depth;
+private:
+};
+
+class HashArray
+{
+public:
+	// Adds element to array and return hash and place
+	std::pair <int, int> addElement(Shape newElement);
+	// Delete element from array by hash and place
+	void removeElement(int hash, int place);
+	// Replace element with this hash and place with new 
+	void replaceElement(int hash, int place, Shape newShape);
+	// Return element by hash and place
+	Shape getElement(int hash, int place);
+	int getSizeOfVector(int hash);
+	// Return hash of element
+	int makeHashOfShape(Shape shapeToHash);
+	std::pair <int, int> findElement();
+protected:
+	std::vector<Shape> arrayOfVectorsOfElements[30];
+private:
+};
